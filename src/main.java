41a80ec7d62c1934770be4efb4c3b62f701adff6
Mark Lysaght <57681015+marklysaght@users.noreply.github.com>
2021-04-10 14:37:02 +0100

    hashmap to store busstops
    function in main to add data to busstop class
    hashmap to store the collection of bus stops stored by name

--- src/main.java
+++ src/main.java
@@ -1,32 +1,56 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File stops = new File("stops.txt");
-		File stop_times = new File("stop_times.txt");
-		File transfers = new File("transfers.txt");
-		TST tst = new TST();
-		try {
-			Scanner sc = new Scanner(stops);
-			sc.next(); // disregard first line as shows the format
-			sc.useDelimiter("\n");
-			while (sc.hasNext()) {
-				String line = sc.next();
-				String[] arr = line.split(",");
-				for(String a: arr) {
-					System.out.println(a);
+
+		
+			HashMap<String,BusStop> stopHash = new HashMap<String,BusStop>();
+			File stops = new File("stops.txt");
+			File stop_times = new File("stop_times.txt");
+			File transfers = new File("transfers.txt");
+			TST tst = new TST();
+			int stop = 1; // for addition to hashMap
+			try {
+				Scanner sc = new Scanner(stops);
+				sc.next(); // disregard first line as shows the format
+				sc.useDelimiter("\n");
+				while (sc.hasNext()) {
+					BusStop x = new BusStop();
+					String line = sc.next();
+					String[] arr = line.split(",");
+					
+					// add elements of arr to busStop object here
+					addToBusStop(arr, x);
+
+					// add to hashMap of BusStop
+					stopHash.put(x.stop_name, x);
+					
 				}
+				sc.close();
+
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			sc.close();
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
-
+	
+	//function to add the details associated with busStop to the busStop object
+	private static void addToBusStop(String[] arr, BusStop x){
+		//TODO
+		x.setStopName(arr[2]);
+		// etc.
+		
+	}
+	
+	
+	
 }
+
+
+
+
